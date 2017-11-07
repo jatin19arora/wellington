271951c4b0947cca88d7e0291239861390187c6f
Ila Gupta <c.ila@yatraonline.local>
2017-11-07 08:10:14 +0530

    file watcher

--- src/main/java/com/sapient/wellington/WellingtonApplication.java
+++ src/main/java/com/sapient/wellington/WellingtonApplication.java
@@ -1,24 +1,34 @@
 package com.sapient.wellington;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sapient.wellington.filereader.FileReader;
 
 @SpringBootApplication
-public class WellingtonApplication implements CommandLineRunner {
+public class WellingtonApplication implements ApplicationRunner {
 
 	@Autowired
 	private FileReader fileReader;
+
+	private static final Logger logger = LoggerFactory.getLogger(WellingtonApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(WellingtonApplication.class, args);
 	}
 
 	@Override
-	public void run(String... arg0) throws Exception {
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("Application started with command-line arguments: {}"+ Arrays.toString(args.getSourceArgs()));
+		System.out.println("NonOptionArgs: {}"+ args.getNonOptionArgs());
+		System.out.printf("OptionNames: {}", args.getOptionNames());
 		fileReader.readFile();
 	}
 
--- src/main/java/com/sapient/wellington/filereader/CSVFileReader.java
+++ src/main/java/com/sapient/wellington/filereader/CSVFileReader.java
@@ -1,5 +1,6 @@
 package com.sapient.wellington.filereader;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
@@ -7,6 +8,8 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.springframework.stereotype.Component;
 
@@ -15,24 +18,50 @@ public class CSVFileReader implements FileReader {
 
 	@Override
 	public void readFile() {
-		Path myDir = Paths.get("D:/data");
-
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		Path myDir = Paths.get("C:/wellington/data");
+		WatchService watcher = null;
 		try {
-			WatchService watcher = myDir.getFileSystem().newWatchService();
+			watcher = myDir.getFileSystem().newWatchService();
 			myDir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		WatchQueueReader fileWatcher = new WatchQueueReader(watcher);
+		executor.execute(fileWatcher);
 
-			WatchKey watckKey = watcher.take();
+	}
 
-			List<WatchEvent<?>> events = watckKey.pollEvents();
-			for (WatchEvent event : events) {
-				if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
-					System.out.println("Created: " + event.context().toString());
+	private class WatchQueueReader implements Runnable {
+		/** the watchService that is passed in from above */
+		private WatchService watcher;
+
+		public WatchQueueReader(WatchService myWatcher) {
+			this.watcher = myWatcher;
+		}
+
+		@Override
+		public void run() {
+			try {
+				WatchKey watchKey = watcher.take();
+				while (watchKey != null) {
+					List<WatchEvent<?>> events = watchKey.pollEvents();
+					for (WatchEvent event : events) {
+						if (event.kind() == StandardWatchEventKinds.ENTRY_CREATE) {
+							System.out.println("Created: " + event.context().toString());
+						}
+					}
+					watchKey.reset();
+					watchKey = watcher.take();
 				}
+
+			} catch (Exception e) {
+				System.out.println("Error: " + e.toString());
 			}
 
-		} catch (Exception e) {
-			System.out.println("Error: " + e.toString());
 		}
+
 	}
 
 }
